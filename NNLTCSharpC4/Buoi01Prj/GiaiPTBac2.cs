57a6e0d536a994553620b0324c6f3cef294a3e22
dntai <donhutai@gmail.com>
2026-09-11 10:19:46 +0700

    Tuan01/Bai GPTBac2 / Minh hoa vscode

---
+++ NNLTCSharpC4/Buoi01Prj/GiaiPTBac2.cs
@@ -0,0 +1,17 @@
+using MyLib; 
+ 
+namespace Buoi01
+{
+    class GiaiPTBac2 
+    { 
+        public static void Main(string[] args) 
+        { 
+            double x1 = 0, x2 = 0; 
+            int sn = LibBaiTap.GiaiPTBac2(1, 5, 6, ref x1, ref x2); 
+    
+            Console.WriteLine($"So nghiem: {sn}"); 
+            Console.WriteLine($"x1 = {x1}"); 
+            Console.WriteLine($"x2 = {x2}"); 
+        } 
+    } 
+}
